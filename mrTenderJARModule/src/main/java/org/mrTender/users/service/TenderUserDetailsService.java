f802d47d5cfdb760510a2b70af376ede243783c1
alander <akhrenov@lanit.ru>
2016-04-10 21:56:28 +0400

    3: Авторизация (Spring+Hibernate)
    Task-Url: https://github.com/alander1984/mrTender/issues/issue/3

--- mrTenderJARModule/src/main/java/org/mrTender/users/service/TenderUserDetailsService.java
+++ mrTenderJARModule/src/main/java/org/mrTender/users/service/TenderUserDetailsService.java
@@ -10,10 +10,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 
 
-@Component
 public class TenderUserDetailsService implements UserDetailsService {
 	
 	UserDao userDao;
